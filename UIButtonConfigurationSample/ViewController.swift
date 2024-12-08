8dd39a58aea013b60a6dd36e7be968eeb23a0b70
KyusokLee <kyusok1@naver.com>
2024-12-08 21:30:06 +0900

    [Add] - add checkButton 1 and 2

--- UIButtonConfigurationSample/ViewController.swift
+++ UIButtonConfigurationSample/ViewController.swift
@@ -2,18 +2,101 @@
 //  ViewController.swift
 //  UIButtonConfigurationSample
 //
-//  Created by gyusoku.i on 2024/12/08.
+//  Created by kyulee on 2024/12/08.
 //
 
 import UIKit
 
+private enum Const {
+    static let normalCheckImage = UIImage(systemName: "checkmark.circle.fill")?.withTintColor(.systemGray3)
+    static let selectedCheckImage = UIImage(systemName: "checkmark.circle.fill")?.withTintColor(.systemGreen.withAlphaComponent(0.7))
+}
+
 class ViewController: UIViewController {
+
+    private lazy var firstCheckButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("チェックボタンその１", for: .normal)
+        button.setTitleColor(UIColor.systemGray2, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 12, weight: .semibold)
+        button.setImage(Const.normalCheckImage?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.setImage(Const.selectedCheckImage?.withRenderingMode(.alwaysOriginal), for: .selected)
+        button.contentHorizontalAlignment = .left
+        button.titleEdgeInsets.left = 10
+        button.imageEdgeInsets = .init(top: 5, left: 5, bottom: 5, right: 0)
+        button.imageView?.contentMode = .scaleAspectFit
+
+        button.addAction(.init { _ in
+            self.firstCheckButton.isSelected.toggle()
+        }, for: .touchUpInside)
+
+        return button
+    }()
+
+    /// Configurationを適用したボタン
+    private lazy var secondCheckButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        var config = UIButton.Configuration.plain()
+        let imageSize = CGSize(width: 20, height: 20)
+        config.title = "チェックボタンその2"
+        config.image = Const.normalCheckImage
+        config.imagePlacement = .leading
+        config.imagePadding = 10
+        config.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 0)
+        config.baseBackgroundColor = .clear
+//        // UIButton.Configurationに書き換えた際、元のimageをそのまま使用すると修正前の画像サイズと一致しないため、画像サイズの再調整を行う
+//        let renderer = UIGraphicsImageRenderer(size: imageSize)
+//        let resizedImage = renderer.image { _ in
+//            Const.normalCheckImage?.draw(in: CGRect(origin: .zero, size: imageSize))
+//        }
+//        config.image = resizedImage
+//        config.imagePadding = 7
+//        config.contentInsets = NSDirectionalEdgeInsets(top: 15, leading: 7, bottom: 15, trailing: 0)
+
+        // ButtonのStateが変わってもfont と titleColorを設定の通りにする
+        config.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+            var outgoing = incoming
+            outgoing.foregroundColor = UIColor.systemGray2
+            outgoing.font = .systemFont(ofSize: 12, weight: .semibold)
+            return outgoing
+        }
+
+        button.contentHorizontalAlignment = .leading
+        button.configuration = config
+        button.addAction(.init { _ in
+            self.secondCheckButton.isSelected.toggle()
+        }, for: .touchUpInside)
+
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.addSubview(firstCheckButton)
+        view.addSubview(secondCheckButton)
+
+        setupUI()
     }
 
+    private func setupUI() {
+        NSLayoutConstraint.activate([
+            // firstCheckButton の制約
+            firstCheckButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            firstCheckButton.heightAnchor.constraint(equalToConstant: 50),
+            firstCheckButton.widthAnchor.constraint(equalToConstant: 150),
+            firstCheckButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
 
+            // secondCheckButton の制約
+            secondCheckButton.topAnchor.constraint(equalTo: firstCheckButton.bottomAnchor, constant: 20),
+            secondCheckButton.heightAnchor.constraint(equalToConstant: 50),
+            secondCheckButton.widthAnchor.constraint(equalToConstant: 150),
+            secondCheckButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+
+
+    }
 }
 
